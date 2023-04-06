ff99e57395f24c691c8b8aa6458d11c81d7fe58d
stsch9 <st.schaubeck@web.de>
2023-04-06 20:26:16 +0200

    add test + class ECVRF

--- src/pyvrf_r255.py
+++ src/pyvrf_r255.py
@@ -12,16 +12,16 @@ q = pow(2, 252) + 27742317777372353535851937790883648493
 base = 'e2f2ae0a6abc4e71a884a961c500515f58e30b6aa582dd8db6a65945e08d2d76'
 
 
-class EcvrfHash:
+class ECVRF:
     def __init__(self, x: bytes):
         if isinstance(x, bytes) and len(x) == 32:
-            self.__x = x
+            self.x = x
             self.Y = crypto_scalarmult_ristretto255_base(x)
         else:
             raise Exception("Invalid key")
 
     @classmethod
-    def generate_keys(cls):
+    def with_ramdom_key(cls):
         x = crypto_core_ristretto255_scalar_random()
         return cls(x)
 
@@ -29,24 +29,29 @@ class EcvrfHash:
     def public_key(self):
         return self.Y
 
-    def _prove(self, alpha: bytes) -> bytes:
+    @property
+    def secret_key(self):
+        return self.x
+
+    def prove(self, alpha: bytes) -> bytes:
         encode_to_curve_salt = self.Y
         H = ECVRF_encode_to_curve(encode_to_curve_salt, alpha)
-        Gamma = crypto_scalarmult_ristretto255(self.__x, H)
+        Gamma = crypto_scalarmult_ristretto255(self.x, H)
 
-        k = ECVRF_nonce_generation(self.__x, H)
+        k = ECVRF_nonce_generation(self.x, H)
         U = crypto_scalarmult_ristretto255_base(k)
         V = crypto_scalarmult_ristretto255(k, H)
 
         c = ECVRF_challenge_generation(self.Y, H, Gamma, U, V)
 
-        s = crypto_core_ristretto255_scalar_add(crypto_core_ristretto255_scalar_mul(c + (16 * b'\x00'), self.__x), k)
+        s = crypto_core_ristretto255_scalar_add(crypto_core_ristretto255_scalar_mul(c + (16 * b'\x00'), self.x), k)
 
         pi = Gamma + c + s
 
         return pi
 
-    def _proof_to_hash(self, pi: bytes) -> bytes:
+    @staticmethod
+    def proof_to_hash(pi: bytes) -> bytes:
         Gamma, c, s = ECVRF_decode_proof(pi)
         proof_to_hash_domain_separator_front = b'\x03'
         proof_to_hash_domain_separator_back = b'\x00'
@@ -56,42 +61,32 @@ class EcvrfHash:
         return beta
 
     def hash(self, alpha: bytes) -> tuple[bytes, bytes]:
-        pi = self._prove(alpha)
-        beta = self._proof_to_hash(pi)
+        pi = self.prove(alpha)
+        beta = self.proof_to_hash(pi)
 
         return pi, beta
 
+    @staticmethod
+    def verify(PK_string: bytes, alpha_string: bytes, pi_string: bytes, validate_key=True) -> bytes:
+        if crypto_core_ristretto255_is_valid_point(PK_string):
+            Y = PK_string
+        else:
+            raise Exception("Invalid point")
 
-def ECVRF_proof_to_hash(pi_string: bytes) -> bytes:
-    Gamma, c, s = ECVRF_decode_proof(pi_string)
-    proof_to_hash_domain_separator_front = b'\x03'
-    proof_to_hash_domain_separator_back = b'\x00'
-    beta_string = sha512(suite_string + proof_to_hash_domain_separator_front + Gamma +
-                         proof_to_hash_domain_separator_back).digest()
+        if validate_key and Y == bytes.fromhex(base):
+            raise Exception("Invalid point")
 
-    return beta_string
+        encode_to_curve_salt = PK_string
+        Gamma, c, s = ECVRF_decode_proof(pi_string)
 
-
-def ECVRF_verify(PK_string: bytes, alpha_string: bytes, pi_string: bytes, validate_key=True) -> bytes:
-    encode_to_curve_salt = PK_string
-    if crypto_core_ristretto255_is_valid_point(PK_string):
-        Y = PK_string
-    else:
-        raise Exception("Invalid point")
-
-    if validate_key and Y == bytes.fromhex(base):
-        raise Exception("Invalid point")
-
-    Gamma, c, s = ECVRF_decode_proof(pi_string)
-
-    H = ECVRF_encode_to_curve(encode_to_curve_salt, alpha_string)
-    U = crypto_core_ristretto255_sub(crypto_scalarmult_ristretto255_base(s), crypto_scalarmult_ristretto255(c + (16 * b'\x00'), Y))
-    V = crypto_core_ristretto255_sub(crypto_scalarmult_ristretto255(s, H),  crypto_scalarmult_ristretto255(c + (16 * b'\x00'), Gamma))
-    c1 = ECVRF_challenge_generation(Y, H, Gamma, U, V)
-    if c == c1:
-        return ECVRF_proof_to_hash(pi_string)
-    else:
-        raise Exception("Verification failed")
+        H = ECVRF_encode_to_curve(encode_to_curve_salt, alpha_string)
+        U = crypto_core_ristretto255_sub(crypto_scalarmult_ristretto255_base(s), crypto_scalarmult_ristretto255(c + (16 * b'\x00'), Y))
+        V = crypto_core_ristretto255_sub(crypto_scalarmult_ristretto255(s, H),  crypto_scalarmult_ristretto255(c + (16 * b'\x00'), Gamma))
+        c1 = ECVRF_challenge_generation(Y, H, Gamma, U, V)
+        if c == c1:
+            return ECVRF.proof_to_hash(pi_string)
+        else:
+            raise Exception("Verification failed")
 
 
 def ECVRF_decode_proof(pi_string: bytes) -> tuple[bytes, bytes, bytes]:
@@ -114,8 +109,7 @@ def ECVRF_decode_proof(pi_string: bytes) -> tuple[bytes, bytes, bytes]:
 def ECVRF_encode_to_curve(encode_to_curve_salt: bytes, alpha_string: bytes) -> bytes:
     encode_to_curve_domain_separator = b'\x82'
     hash_string = sha512(suite_string + encode_to_curve_domain_separator + encode_to_curve_salt + alpha_string).digest()
-    print("hash_string:  " + hash_string.hex())
-    #return sodium.pnt(hash_string)
+
     return crypto_core_ristretto255_from_hash(hash_string)
 
 
@@ -123,8 +117,7 @@ def ECVRF_encode_to_curve(encode_to_curve_salt: bytes, alpha_string: bytes) -> b
 def ECVRF_nonce_generation(SK: bytes, h_string: bytes) -> bytes:
     nonce_generation_domain_separator = b'\x81'
     k_string = sha512(suite_string + nonce_generation_domain_separator + SK + h_string).digest()
-    print("k_string: " + k_string.hex())
-    #return sodium.red(k_string)
+
     return crypto_core_ristretto255_scalar_reduce(k_string)
 
 
@@ -135,9 +128,5 @@ def ECVRF_challenge_generation(P1: bytes, P2: bytes, P3: bytes, P4: bytes, P5: b
           challenge_generation_domain_separator_back
 
     c_string = sha512(str).digest()
-    print("c_string: " + c_string.hex())
 
     return c_string[0:cLen]
-
-
-print(EcvrfHash.generate_keys().hash(b"bla"))
--- test/test_pyvrf-r255.py
+++ test/test_pyvrf-r255.py
@@ -2,6 +2,7 @@ from pysodium import crypto_scalarmult_ristretto255_base, crypto_scalarmult_rist
     crypto_core_ristretto255_from_hash, crypto_core_ristretto255_scalar_reduce, crypto_core_ristretto255_scalar_mul, \
     crypto_core_ristretto255_scalar_add, crypto_core_ristretto255_is_valid_point, crypto_core_ristretto255_sub
 from hashlib import sha512
+from src.pyvrf_r255 import ECVRF
 
 SK = '3431c2b03533e280b23232e280b34e2c3132c2b03238e280b23131e280b34500'
 alpha = '633273702e6f72672f7672662d72323535'
@@ -15,7 +16,6 @@ base = 'e2f2ae0a6abc4e71a884a961c500515f58e30b6aa582dd8db6a65945e08d2d76'
 
 def ECVRF_prove(SK: bytes, alpha_string: bytes):
     x = SK
-    #Y = sodium.bas(SK)
     Y = crypto_scalarmult_ristretto255_base(x)
     print("Y: " + Y.hex())
     encode_to_curve_salt = Y
@@ -24,18 +24,15 @@ def ECVRF_prove(SK: bytes, alpha_string: bytes):
     print("H: " + H.hex())
     h_string = H
 
-    #Gamma = sodium.mul(x, H)
     Gamma = crypto_scalarmult_ristretto255(x, H)
     print("Gamma: " + Gamma.hex())
 
     k = ECVRF_nonce_generation(SK, h_string)
     print("k: " + k.hex())
 
-    #U = sodium.bas(k)
     U = crypto_scalarmult_ristretto255_base(k)
     print("U: " + U.hex())
 
-    #V = sodium.mul(k,H)
     V = crypto_scalarmult_ristretto255(k, H)
     print("V: " + V.hex())
 
@@ -104,7 +101,7 @@ def ECVRF_encode_to_curve(encode_to_curve_salt: bytes, alpha_string: bytes) -> b
     encode_to_curve_domain_separator = b'\x82'
     hash_string = sha512(suite_string + encode_to_curve_domain_separator + encode_to_curve_salt + alpha_string).digest()
     print("hash_string:  " + hash_string.hex())
-    #return sodium.pnt(hash_string)
+
     return crypto_core_ristretto255_from_hash(hash_string)
 
 
@@ -113,24 +110,31 @@ def ECVRF_nonce_generation(SK: bytes, h_string: bytes) -> bytes:
     nonce_generation_domain_separator = b'\x81'
     k_string = sha512(suite_string + nonce_generation_domain_separator + SK + h_string).digest()
     print("k_string: " + k_string.hex())
-    #return sodium.red(k_string)
+
     return crypto_core_ristretto255_scalar_reduce(k_string)
 
 
 def ECVRF_challenge_generation(P1: bytes, P2: bytes, P3: bytes, P4: bytes, P5: bytes) -> bytes:
     challenge_generation_domain_separator_front = b'\x02'
     challenge_generation_domain_separator_back = b'\x00'
-    str = suite_string + challenge_generation_domain_separator_front + P1 + P2 + P3 + P4 + P5 + \
-          challenge_generation_domain_separator_back
+    s = suite_string + challenge_generation_domain_separator_front + P1 + P2 + P3 + P4 + P5 + \
+        challenge_generation_domain_separator_back
 
-    c_string = sha512(str).digest()
+    c_string = sha512(s).digest()
     print("c_string: " + c_string.hex())
 
     return c_string[0:cLen]
 
 
+print("Test vector (see https://github.com/C2SP/C2SP/blob/main/vrf-r255.md#test-vector)")
+SK = '3431c2b03533e280b23232e280b34e2c3132c2b03238e280b23131e280b34500'
+alpha = '633273702e6f72672f7672662d72323535'
+PK = crypto_scalarmult_ristretto255_base(bytes.fromhex(SK))
+print("SK: " + SK)
+print("PK: " + PK.hex())
+print("alpha: " + alpha)
 
-
+print("---hash---")
 pi = ECVRF_prove(bytes.fromhex(SK), bytes.fromhex(alpha))
 print('pi: ' + pi.hex())
 beta = ECVRF_proof_to_hash(pi)
@@ -138,3 +142,14 @@ print('beta: ' + beta.hex())
 print("---verify---")
 beta_string = ECVRF_verify(PK, bytes.fromhex(alpha), pi)
 print("beta: " + beta_string.hex())
+
+print("-------------------")
+print("Test Class ECVRF:")
+vrf = ECVRF(bytes.fromhex(SK))
+pk = vrf.public_key
+print("PK: " + pk.hex())
+pi, beta = vrf.hash(bytes.fromhex(alpha))
+print('pi: ' + pi.hex())
+print('beta: ' + beta.hex())
+
+print(vrf.verify(pk, bytes.fromhex(alpha), pi, beta).hex())
